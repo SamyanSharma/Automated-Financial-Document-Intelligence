2d3f3d5d06caa12090a40c36bb9ad93730666cfb
Prakriti <sthaprak@gmail.com>
2026-08-12 11:39:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,4 +12,10 @@ export default defineConfig({
       },
     },
   },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.js'],
+    globals: true,
+    css: false,
+  },
 });
